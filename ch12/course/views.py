157cff130aa770263efad99e8fb5269f6a3815fe
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-02-03 07:04:27 +0600

    learn: django template language

--- ch12/course/views.py
+++ ch12/course/views.py
@@ -27,6 +27,32 @@ def home(req):
 #     return render(req,'course/django.html',couse_details)
 
 # example 1.2 Date
+# def learn_django(req):
+#     date = datetime.datetime.now()
+#     return render(req,'course/django.html',{'dt': date})
+
+
+# example 2 float format
+# def learn_django(req):
+#     nums = {'v1': 96.278, 'v2': 96.000, 'v3': 96.3700}
+#     return render(req,'course/django.html',nums)
+
+# # example 3 If else
+
+# def learn_django(req):
+#     info = {'name': 'Django', 'version': '3.5','available': True}
+#     return render(req,'course/django.html',info)
+
+# # example 4 for loop and for loop counter
+
 def learn_django(req):
-    date = datetime.datetime.now()
-    return render(req,'course/django.html',{'dt': date})
+    courses = [
+        {"name": "Django Basics", "duration": "3 weeks", "level": "Beginner"},
+        {"name": "Advanced Django", "duration": "6 weeks", "level": "Intermediate"},
+        {"name": "REST APIs with DRF", "duration": "4 weeks", "level": "Intermediate"},
+        {"name": "Full-Stack with Django & React", "duration": "8 weeks", "level": "Advanced"},
+    ]
+        
+
+
+    return render(req,'course/django.html',{'courses': courses})
