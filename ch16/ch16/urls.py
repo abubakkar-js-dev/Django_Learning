c328e4789b2dce1c1e91c8fd56abc369180e4b49
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-02-11 06:33:39 +0600

    add: django-bootstrap-5 in django project

---
+++ ch16/ch16/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from core.views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('course/',include('course.urls')),
+]
