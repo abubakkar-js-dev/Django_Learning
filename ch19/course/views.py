c4f2d3b2258fc0d2886efa6f58af79d733a28e12
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-02-15 07:07:39 +0600

    updated

---
+++ ch19/course/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def learn_django(req):
+    return render(req,'course/django.html',content_type='text/html')
+
+def learn_python(req):
+    return render(req,'course/python.html',content_type='text/html')
