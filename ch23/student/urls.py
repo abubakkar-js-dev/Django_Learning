b0e9baec727eb58b000e9b05d37c3e352284619d
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-03-05 17:46:28 +0600

    add: all students and student details page with logic

--- ch23/student/urls.py
+++ ch23/student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from student.views import all_students
+from student.views import all_students,studentDetails
 
 urlpatterns = [
-    path('',all_students,name='all_students')
+    path('',all_students,name='all_students'),
+    path('<int:id>/',studentDetails,name='student_details'),
 ]
--- ch23/student/views.py
+++ ch23/student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from student.models import Profile
 
 # Create your views here.
@@ -6,3 +6,11 @@ from student.models import Profile
 def all_students(req):
     students = Profile.objects.all()
     return render(req,'student/allStudents.html',{'students': students})
+
+
+def studentDetails(req,id):
+    # student=Profile.objects.get(id=id)
+    student=get_object_or_404(Profile,id=id)
+     
+    
+    return render(req,'student/studentDetails.html',{'student': student})
