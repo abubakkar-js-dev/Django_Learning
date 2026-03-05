e85e395ee0e48fb6eabbedca4571c021893b9e60
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-03-05 16:29:08 +0600

    add: django model for ch23

--- ch22/student/models.py
+++ ch22/student/models.py
@@ -4,12 +4,17 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length = 75)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, max_length=255)
     city = models.CharField(max_length=70)
     roll = models.IntegerField()
+    state = models.CharField(max_length=70)
+    comment = models.CharField(max_length=70,default='This is default comment')
+
     
 
 
 # python manage.py makemigrations
 
 # python manage.py migrate
+
+# python manage.py sqlmigrate student 0001
